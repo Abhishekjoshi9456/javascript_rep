51bd0b4967c190a3a01ec8e9a28a3b8ad68471e7
Abhishek Joshi <113884194+Abhishekjoshi9456@users.noreply.github.com>
2024-10-09 16:44:06 +0000

    datatype

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,15 @@
+const account_id = 12928                        // const is not change
+let account_email = "abhishek123321@gmail.com"  // changable
+var account_pass = "12345"                      // var not understand Scope
+account_city = "uttarakhand"
+let account_State;
+
+// account_id = 102          // Not allowed
+
+account_email = "abhi@gmial.com"
+account_pass = "23441"
+account_city = "vadodara"
+
+
+console.log(account_id)
+console.table([account_id, account_email, account_pass, account_city, account_State])
