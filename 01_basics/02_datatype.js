f657cebf302de6f39de2e6f89e08b6a3da89b3ff
Abhishek Joshi <113884194+Abhishekjoshi9456@users.noreply.github.com>
2024-10-09 17:05:33 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,20 @@
+let int = 20
+let string = "abhishek"
+let boolean = true
+
+
+let x = null    // object datatype
+let y;          // undefined datatype
+
+// number 
+// bigint 
+// string
+// boolean
+// null
+// undefined 
+
+
+// object
+
+console.log(typeof x)
+console.log(typeof y)
